1c661c998d0cafc886519f4ba9347fb417c75059
Thu Nguyen <thu.nguyen@smartdev.vn>
2018-11-09 17:17:18 +0700

    add server and pm2 json to deploy on server

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -8,11 +8,8 @@ export const OBJECT_COMPONENTS = {
   HomeComponent
 };
 
-export const ALL_COMPONENTS = importToArray(OBJECT_COMPONENTS);
-
-export function importToArray<Key extends string, PropType>(importObject: Record<Key, PropType>): PropType[] {
-  const keys = Object.getOwnPropertyNames(importObject);
-
-  // ES6 / TypeScript exports contain a __esModule property. Don't include that.
-  return keys.filter(key => key.indexOf("__") !== 0).map(key => importObject[key]);
-}
+export const ALL_COMPONENTS = [
+  DashboardLayoutComponent,
+  LoginComponent,
+  HomeComponent
+];
