fbc4d3fb59d6fffc810b3e480d0f6737940849c2
quangtuandev <quangtuandev@gmail.com>
2019-03-04 17:31:59 +0700

    update status for comany

---
+++ src/app/components/companies/company-detail/company-detail.component.ts
@@ -0,0 +1,58 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import map from 'lodash/map';
+import { ConfirmDialogComponent } from '../../../directives';
+import { JobsService } from '../../../services';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'company-detail',
+  templateUrl: 'company-detail.component.html',
+  styleUrls: ['company-detail.component.scss']
+})
+export class CompanyDetailComponent implements OnInit {
+  @ViewChild('confirmJobStatusDialog') confirmJobStatusDialog: ConfirmDialogComponent;
+
+  jobDetail: any = {};
+
+  constructor(
+    private jobsService: JobsService,
+    private route: ActivatedRoute,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.getListJobs();
+  }
+
+  getListJobs() {
+    const jobId = this.route.snapshot.paramMap.get('id');
+    this.jobsService.getJobDetail(jobId).subscribe(
+      data => {
+        this.jobDetail = data;
+      },
+      error => {
+      }
+    );
+  }
+
+  changeJobStatus(status: string, jobId: number) {
+    this.confirmJobStatusDialog.openModal({status, jobId});
+  }
+
+  handleConfirm(isConfirm: any) {
+    if (isConfirm) {
+      const { jobId, status } = isConfirm;
+      this.jobsService.updateJobStatus(jobId, status).subscribe(
+        data => {
+          this.jobDetail.status = data.status;
+          this.toastr.success(`The job was updated successfully.`, 'Update Job');
+        },
+        error => {
+          this.toastr.error('Update Failed!', 'Update Job');
+        }
+      );
+    }
+  }
+
+}
